aab77ab4e7746ef8729a9697b5107d9bb1e7df08
Akshaysingh Sanjaysingh Bayes <akshaybayes95@gmail.com>
2024-08-29 23:01:41 -0400

    Time: 1115 ms (78.95%), Space: 0B (100%) - LeetHub

--- 1153-product-sales-analysis-i/1153-product-sales-analysis-i.sql
+++ 1153-product-sales-analysis-i/1153-product-sales-analysis-i.sql
@@ -1,4 +1,4 @@
 # Write your MySQL query statement below
 SELECT p.product_name, s.year, s.price
-From Sales s Left Join Product p
-on s.product_id = p.product_id;
+From Sales s, Product p
+where s.product_id = p.product_id;
