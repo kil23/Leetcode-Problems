332ceea192f27ac580c8dc817e66789f0966d2fd
Akshaysingh Sanjaysingh Bayes <akshaybayes95@gmail.com>
2024-09-04 03:13:13 -0400

    Time: 675 ms (72.47%), Space: 0B (100%) - LeetHub

---
+++ 1390-average-selling-price/1390-average-selling-price.sql
@@ -0,0 +1,6 @@
+# Write your MySQL query statement below
+Select p.product_id, COALESCE(ROUND(SUM(p.price*u.units)/SUM(u.units), 2), 0) as average_price
+from Prices p left join Unitssold u
+on p.product_id = u.product_id and 
+u.purchase_date between p.start_date and p.end_date
+group by p.product_id;
