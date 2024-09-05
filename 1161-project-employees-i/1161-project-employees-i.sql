3bd18aef9d527e5bfcb90102b42bbb3e38db86ab
Akshaysingh Sanjaysingh Bayes <akshaybayes95@gmail.com>
2024-09-05 02:48:46 -0400

    Time: 498 ms (62.07%), Space: 0B (100%) - LeetHub

--- 1161-project-employees-i/1161-project-employees-i.sql
+++ 1161-project-employees-i/1161-project-employees-i.sql
@@ -1,5 +1,5 @@
 # Write your MySQL query statement below
-Select p.project_id, ROUND(AVG(e.experience_years),2) as average_years
-from Project p Left join Employee e
-on p.employee_id = e.employee_id
-Group by p.project_id;
+Select project_id, ROUND(AVG(experience_years),2) AS average_years FROM
+Project LEFT OUTER JOIN Employee
+ON Project.employee_id=Employee.employee_id
+GROUP BY project_id;
