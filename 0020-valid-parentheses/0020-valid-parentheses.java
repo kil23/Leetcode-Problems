66d8adebce70e7778b7e58a5f05803336a679326
Akshaysingh Sanjaysingh Bayes <akshaybayes95@gmail.com>
2025-08-22 18:07:16 -0400

    Time: 3 ms (37.78%), Space: 41.4 MB (97.16%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -0,0 +1,24 @@
+class Solution {
+    public boolean isValid(String s) {
+        HashMap<Character, Character> mappings = new HashMap<>();
+        mappings.put(')', '(');
+        mappings.put(']', '[');
+        mappings.put('}', '{');
+
+        Stack<Character> st = new Stack<>();
+
+        for(int i=0; i<s.length(); i++) {
+            char c = s.charAt(i);
+
+            if(mappings.containsKey(c)) {
+                char topElement = st.empty() ? '#' : st.pop();
+                if(topElement != mappings.get(c)) return false;
+            }else{
+                st.push(c);
+            }
+        }
+
+        return st.isEmpty();
+        
+    }
+}
