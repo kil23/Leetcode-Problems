ca2269ea6c6f7b67194bec86bddcf27929ac310b
Akshaysingh Sanjaysingh Bayes <akshaybayes95@gmail.com>
2025-09-08 20:30:37 -0400

    Time: 0 ms (100%), Space: 55.9 MB (89.66%) - LeetHub

--- 0882-peak-index-in-a-mountain-array/0882-peak-index-in-a-mountain-array.java
+++ 0882-peak-index-in-a-mountain-array/0882-peak-index-in-a-mountain-array.java
@@ -1,19 +1,17 @@
 class Solution {
     public int peakIndexInMountainArray(int[] arr) {
-        int start = 1, end = arr.length - 2;
+        int start = 0, end = arr.length - 1;
 
-        while(start <= end) {
+        while(start < end) {
             int mid = start + (end - start)/2;
 
-            if(arr[mid-1] < arr[mid] && arr[mid] > arr[mid+1]) {
-                return mid;
-            }else if(arr[mid-1]<arr[mid]) {
+            if(arr[mid] < arr[mid+1]) {
                 start = mid + 1;
             }else {
-                end = mid - 1;
+                end = mid;
             }
         }
 
-        return -1;
+        return start;
     }
 }
