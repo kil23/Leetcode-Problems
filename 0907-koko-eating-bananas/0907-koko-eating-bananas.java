61ade7dc2bb3d09aa968c88fc98f7e5f356759fd
Akshaysingh Sanjaysingh Bayes <akshaybayes95@gmail.com>
2025-09-10 14:17:37 -0400

    Time: 32 ms (6.91%), Space: 45.6 MB (9.01%) - LeetHub

--- 0907-koko-eating-bananas/0907-koko-eating-bananas.java
+++ 0907-koko-eating-bananas/0907-koko-eating-bananas.java
@@ -1,34 +1,28 @@
 class Solution {
-
-    public boolean isPossible(int mid, int[] piles, int h) {
-        int hours = 0;
-        int n = piles.length;
-
+     public boolean isPossible(int mid, int[] piles, int h) {
+        long countHr = 0;
         for(int pile: piles) {
-            hours +=  (int) (Math.ceil((double)pile/mid));
+            countHr += (int)Math.ceil((double)pile/mid);
         }
-
-        return hours<=h;
+        System.out.println(countHr);
+        return h >= countHr;
     }
 
     public int minEatingSpeed(int[] piles, int h) {
-        int n = piles.length;
-        int low = 1;
+        int low = 0;
         int high = Integer.MAX_VALUE;
-
         int ans = -1;
 
         while(low<=high) {
-            int mid = low + (high-low)/2;
+            int mid = low + (high - low)/2;
             if(isPossible(mid, piles, h)) {
                 ans = mid;
                 high = mid - 1;
-            }else{
+            }else {
                 low = mid + 1;
             }
         }
 
         return ans;
-
     }
 }
