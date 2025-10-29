42ffadb70517b684384d6d094010984e25b48314
Akshaysingh Sanjaysingh Bayes <akshaybayes95@gmail.com>
2025-10-29 03:35:45 -0400

    Time: 7 ms (55.61%), Space: 42.3 MB (33.79%) - LeetHub

---
+++ 2053-check-if-all-characters-have-equal-number-of-occurrences/2053-check-if-all-characters-have-equal-number-of-occurrences.java
@@ -0,0 +1,12 @@
+class Solution {
+    public boolean areOccurrencesEqual(String s) {
+        Map<Character, Integer> map = new HashMap<>();
+        for(char ch : s.toCharArray()) {
+            map.put(ch, map.getOrDefault(ch, 0) + 1);
+        }
+
+        Set<Integer> set = new HashSet<>(map.values());
+
+        return set.size() == 1;
+    }
+}
