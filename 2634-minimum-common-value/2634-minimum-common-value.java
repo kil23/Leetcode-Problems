32fec9acfb67d3e610d19655b96822618377af56
Akshaysingh Sanjaysingh Bayes <akshaybayes95@gmail.com>
2025-10-29 04:00:14 -0400

    Time: 2 ms (88.07%), Space: 62.7 MB (60.81%) - LeetHub

---
+++ 2634-minimum-common-value/2634-minimum-common-value.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int getCommon(int[] nums1, int[] nums2) {
+        int first = 0;
+        int second = 0;
+
+        while(first < nums1.length && second < nums2.length) {
+            if(nums1[first] == nums2[second]) {
+                return nums1[first];
+            } else if(nums1[first] < nums2[second]) {
+                first++;
+            } else {
+                second++;
+            }
+        }
+
+        return -1;
+    }
+}
