063c4628cfe0f94f883648dcdb56a9ce6d2bbd41
Akshaysingh Sanjaysingh Bayes <akshaybayes95@gmail.com>
2025-11-03 21:55:34 -0500

    Time: 13 ms (81.34%), Space: 47.8 MB (5.09%) - LeetHub

--- 1171-shortest-path-in-binary-matrix/1171-shortest-path-in-binary-matrix.java
+++ 1171-shortest-path-in-binary-matrix/1171-shortest-path-in-binary-matrix.java
@@ -1,42 +1,41 @@
 class Solution {
     public int shortestPathBinaryMatrix(int[][] grid) {
-        if(grid[0][0] != 0 || grid[grid.length - 1][grid[0].length - 1] != 0) {
-            return -1;
-        }
+        int n = grid.length;
+        int m = grid[0].length;
+       
+        if(grid[0][0] != 0 || grid[n-1][m-1] != 0) return -1;
 
         int[][] directions = {
-            {-1, -1}, {-1, 0}, {-1, 1}, 
+            {-1, -1}, {-1, 0}, {-1, 1},
             {0, -1}, {0, 1}, 
-            {1, -1}, {1, 0}, {1, 1} 
+            {1, -1}, {1, 0}, {1, 1}
         };
 
-            Queue<int[]> queue = new LinkedList<>();
-            queue.add(new int[]{0, 0, 1});
-            grid[0][0] = 1;
+        Queue<int[]> queue = new LinkedList<>();
+        queue.add(new int[]{0, 0, 1});
+        grid[0][0] = 1;
 
-            while(!queue.isEmpty()) {
-                int[] cell = queue.remove();
-                int row = cell[0];
-                int col = cell[1];
-                int steps = cell[2];
+        while(!queue.isEmpty()) {
+            int[] cell = queue.remove();
+            int row = cell[0];
+            int col = cell[1];
+            int steps = cell[2];
 
-                if(row == grid.length - 1 && col == grid[0].length - 1) {
-                    return steps;
-                }
+            if(row == n-1 && col == m-1) return steps;
 
-                for(int[] direction : directions) {
-                    int newRow = row + direction[0];
-                    int newCol = col + direction[1];
-                    
-                    if(newRow >= 0 && newRow < grid.length && 
-                    newCol >= 0 && newCol < grid[0].length &&
+            for(int[] direction: directions) {
+                int newRow = row + direction[0];
+                int newCol = col + direction[1];
+
+                if(newRow >=0 && newRow < n &&
+                    newCol >= 0 && newCol < m &&
                     grid[newRow][newCol] == 0) {
                         grid[newRow][newCol] = 1;
-                        queue.add(new int[] {newRow, newCol, steps + 1});
-                    }
+                        queue.add(new int[]{newRow, newCol, steps+1});
                 }
             }
+        }
 
-            return -1;
+        return -1;
     }
 }
