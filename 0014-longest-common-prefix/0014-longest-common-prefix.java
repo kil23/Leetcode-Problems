69b2f879fe209c99ccc903981665dfea394d76f6
Akshaysingh Sanjaysingh Bayes <akshaybayes95@gmail.com>
2025-11-03 21:45:06 -0500

    Time: 0 ms (100%), Space: 43.2 MB (5.09%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.java
+++ 0014-longest-common-prefix/0014-longest-common-prefix.java
@@ -3,10 +3,20 @@ class Solution {
         if(strs.length == 0) return "";
 
         String prefix = strs[0];
+        int prefixLen = prefix.length();
 
         for(int i=1; i<strs.length; i++) {
-            while(strs[i].indexOf(prefix) != 0) {
-                prefix = prefix.substring(0, prefix.length() - 1);
+            // while(strs[i].indexOf(prefix) != 0) {
+            //     prefix = prefix.substring(0, prefix.length() - 1);
+            // }
+            String s = strs[i];
+            while(prefixLen > s.length() || !prefix.equals(s.substring(0, prefixLen))) {
+                prefixLen--;
+                if(prefixLen == 0) {
+                    return "";
+                }
+
+                prefix = prefix.substring(0, prefixLen);
             }
         }
 
