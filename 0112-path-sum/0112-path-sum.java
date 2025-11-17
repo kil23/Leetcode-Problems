2cc1857295ca241d1f647f7a0dce49a3f32dc7f9
Akshaysingh  Bayes <akshaybayes95@gmail.com>
2025-11-17 02:21:31 -0500

    Time: 0 ms (100%), Space: 44.8 MB (15.27%) - LeetHub

---
+++ 0112-path-sum/0112-path-sum.java
@@ -0,0 +1,32 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public boolean hasPathSum(TreeNode root, int targetSum) {
+        return dfs(root, targetSum, 0);
+    }
+
+    private boolean dfs(TreeNode node, int target, int curr) {
+        if(node == null) return false;
+
+        curr += node.val;
+
+        if(node.left == null && node.right == null) {
+            return target == curr;
+        } 
+
+        return dfs(node.left, target, curr) || dfs(node.right, target, curr);
+    }
+}
