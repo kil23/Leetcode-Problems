4a7da60613f815664fb081bd0ae5c714b6871dc6
Akshaysingh  Bayes <akshaybayes95@gmail.com>
2026-02-02 11:15:37 -0800

    Time: 2 ms (99.76%), Space: 52.4 MB (41.84%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.java
+++ 0200-number-of-islands/0200-number-of-islands.java
@@ -1,29 +1,53 @@
 class Solution {
     public int numIslands(char[][] grid) {
+    //     int n = grid.length;
+    //     int m = grid[0].length;
+
+    //     int islands = 0;
+    //     for(int i=0; i<n; i++) {
+    //         for(int j=0; j<m; j++) {
+    //             if(grid[i][j] == '1') {
+    //                 islands++;
+    //                 dfs(i, j, grid);
+    //             }
+    //         }
+    //     }
+
+    //     return islands;
+    // }
+
+    // private void dfs(int i, int j, char[][] grid) {
+    //     if(i < 0 || i >= grid.length || j < 0 || j >= grid[0].length || grid[i][j] == '0') return;
+
+    //     grid[i][j] = '0';
+
+    //     dfs(i+1, j, grid);
+    //     dfs(i-1, j, grid);
+    //     dfs(i, j+1, grid);
+    //     dfs(i, j-1, grid);
+
+        int count = 0;
         int n = grid.length;
         int m = grid[0].length;
 
-        int islands = 0;
         for(int i=0; i<n; i++) {
             for(int j=0; j<m; j++) {
                 if(grid[i][j] == '1') {
-                    islands++;
-                    dfs(i, j, grid);
+                    count++;
+                    callbfs(grid, i, j);
                 }
             }
         }
-
-        return islands;
+        return count;
     }
 
-    private void dfs(int i, int j, char[][] grid) {
-        if(i < 0 || i >= grid.length || j < 0 || j >= grid[0].length || grid[i][j] == '0') return;
+    public void callbfs(char[][] grid, int i, int j) {
+        if(i<0 || i>=grid.length || j<0 || j>=grid[i].length || grid[i][j] == '0') return;
 
         grid[i][j] = '0';
-
-        dfs(i+1, j, grid);
-        dfs(i-1, j, grid);
-        dfs(i, j+1, grid);
-        dfs(i, j-1, grid);
+        callbfs(grid, i+1, j);
+        callbfs(grid, i-1, j);
+        callbfs(grid, i, j+1);
+        callbfs(grid, i, j-1);
     }
 }
